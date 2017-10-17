41eec0c5c799d203a67206101c9af75857717813
Chaelgutierrez <michael@vielsoft.com>
2017-10-17 13:04:52 +0800

    Added main api server script

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var cors = require('cors');
+var helmet = require('helmet');
+var ip = require('ip');
+var port = process.env.Port || 3100;
+var host = ip.address();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.get('/api', function(req, res) {
+    res.send("Initialized server deployer api");
+});
+
+app.listen(port, host, function() {
+    console.log("Listening to " + host + ":" + port);
+});
